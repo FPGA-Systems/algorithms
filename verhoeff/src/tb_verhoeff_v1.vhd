9f3e2fd597634768dd8254cf60d9b288ebb0dd27
fpgasystems <FPGA-Systems@yandex.ru>
2023-07-15 21:15:56 +0300

    initial commit

---
+++ verhoeff/src/tb_verhoeff_v1.vhd
@@ -0,0 +1,102 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+library work;
+use work.all;
+
+entity tb_verhoeff_v1 is
+end tb_verhoeff_v1;
+
+architecture sim of tb_verhoeff_v1 is
+
+    type sequence_type is array (natural range <>) of natural range 0 to 9;
+    constant s0 : sequence_type := (2,3,6);--3
+    constant s1 : sequence_type := (0,1,0,4,2,0,1,7);--3
+    constant s2 : sequence_type := (1,3,2,1,5,1,8,9,1,8,1,5,1,1,3,2,1,6,5,1,9,8,7,7,8,4,6,5,1,3,2,1,6,5,4,9,8,7,9,5,6,1,3,2,1,5,6,4,8,9,4,8,4);--9
+    constant s3 : sequence_type := (0,9,8,1,2,3,7,4,0,9,8,1,7,5,3,4,2,0,5,9,8,7);--8
+    
+    signal clk : std_logic := '1';
+    constant clk_period : time := 10ns;
+    signal reset : std_logic := '0';
+    
+    signal istart    : std_logic;
+    signal idigit    : std_logic_vector(3 downto 0);
+    signal idigit_valid : std_logic;
+    signal idone     : std_logic;
+    
+    signal odone            : std_logic;
+    signal ocontrol_digit   : std_logic_vector(3 downto 0);
+    signal oready           : std_logic;
+    
+    procedure execute (
+        constant s : in sequence_type;
+        signal start   : out std_logic;
+        signal digit   : out std_logic_vector(3 downto 0);
+        signal digit_valid : out std_logic;
+        signal done    : out std_logic
+    ) is
+    
+    begin
+        
+        start <= '0';
+        digit <= (others => '0');
+        digit_valid <= '0';
+        done <= '0';
+        
+        wait for 3*clk_period;
+        
+        start <= '1';
+        
+        for i in s'reverse_range loop
+            digit_valid <= '1';
+            digit <= std_logic_vector(to_unsigned(s(i), digit'length));
+            
+            if i = 0 then done <= '1'; end if;
+            
+            wait for clk_period;
+            start <= '0';
+        end loop;
+        
+        done <= '0';
+        digit_valid <= '0';
+        digit <= std_logic_vector(to_unsigned(0, digit'length));
+        
+    end procedure;
+begin
+    
+    clk <= not clk after clk_period/2;
+    
+    process
+    begin
+        wait for 10*clk_period;
+        reset <= '1';
+        wait for 3*clk_period;
+        reset <= '0';
+        
+        execute(s0, istart, idigit, idigit_valid, idone);
+        execute(s1, istart, idigit, idigit_valid, idone);
+        execute(s2, istart, idigit, idigit_valid, idone);
+        execute(s3, istart, idigit, idigit_valid, idone);
+        wait;
+    end process;
+    
+    dut: entity work.verhoeff_v1(rtl)
+    port map(
+        iclk    => clk,
+        ireset  => reset,
+        
+        idigit       => idigit,
+        idigit_valid => idigit_valid,
+        
+        istart => istart,
+        idone  => idone,
+        
+        odone          => odone,
+        ocontrol_digit => ocontrol_digit,
+        oready         => oready
+    );
+    
+end sim;
---
+++ verhoeff/src/verhoeff_checker_v1.vhd
@@ -0,0 +1,138 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+
+entity verhoeff_checker_v1 is
+    port (
+        iclk    : in std_logic;
+        ireset  : in std_logic;
+        
+        idigit  : in std_logic_vector(3 downto 0);
+        idigit_valid    : in std_logic;
+        
+        istart  : in std_logic;
+        idone    : in std_logic;
+        
+        odone   : out std_logic;
+        ocorrect: out std_logic;
+        oerror  : out std_logic;
+        oready  : out std_logic
+    );
+end verhoeff_checker_v1;
+
+architecture rtl of verhoeff_checker_v1 is
+    type num_type is array (0 to 9) of natural range 0 to 9;
+    type p_type is array  (0 to 7) of num_type;
+        
+    signal p: p_type := (
+        (0,	1,	2,	3,	4,	5,	6,	7,	8,	9),
+        (1,	5,	7,	6,	2,	8,	3,	0,	9,	4),
+        (5,	8,	0,	3,	7,	9,	6,	1,	4,	2),
+        (8,	9,	1,	6,	0,	4,	3,	5,	2,	7),
+        (9,	4,	5,	3,	1,	2,	6,	8,	7,	0),
+        (4,	2,	8,	6,	5,	7,	3,	9,	0,	1),
+        (2,	7,	9,	3,	8,	0,	6,	4,	1,	5),
+        (7,	0,	4,	6,	9,	1,	3,	2,	5,	8)
+    );
+    
+    type d_type is array (0 to 9) of num_type;
+    signal d : d_type := (
+        (0,	1,	2,	3,	4,	5,	6,	7,	8,	9),
+        (1,	2,	3,	4,	0,	6,	7,	8,	9,	5),
+        (2,	3,	4,	0,	1,	7,	8,	9,	5,	6),
+        (3,	4,	0,	1,	2,	8,	9,	5,	6,	7),
+        (4,	0,	1,	2,	3,	9,	5,	6,	7,	8),
+        (5,	9,	8,	7,	6,	0,	4,	3,	2,	1),
+        (6,	5,	9,	8,	7,	1,	0,	4,	3,	2),
+        (7,	6,	5,	9,	8,	2,	1,	0,	4,	3),
+        (8,	7,	6,	5,	9,	3,	2,	1,	0,	4),
+        (9,	8,	7,	6,	5,	4,	3,	2,	1,	0)
+    );
+    
+    signal inv: num_type := (0, 4, 3, 2, 1, 5, 6, 7, 8, 9);
+    
+    type state_type is (s0, s1, s2, s3, s4);
+    signal state : state_type := s0;
+    
+    signal k : std_logic_vector(2 downto 0) := b"000";
+    
+    signal c : natural range 0 to 9 := 0;
+    
+begin
+    
+    process(iclk)
+        variable p_value : natural range 0 to 9;
+    begin
+        
+        if rising_edge(iclk) then
+            if ireset = '1' then
+                odone <= '0';
+                oerror <= '0';
+                ocorrect <= '0';
+                oready <= '1';
+                
+                state <= s0;
+                k <= b"000";
+                c <= 0;
+            else
+                
+                case state is
+                    when s0 =>
+                        k <= b"000";
+                        odone <= '0';
+                        oerror <= '0';
+                        ocorrect <= '0';
+                        oready <= '1';
+                        c <= 0;
+                        
+                        if istart = '1' then
+                            if idigit_valid = '1' then
+                                c <= to_integer(unsigned(idigit));
+                                k <= k + '1';
+                                state <= s2;
+                            else
+                                state <= s1;
+                            end if;
+                            
+                        end if;
+                   
+                   when s1 =>
+                        if idigit_valid = '1' then
+                            c <= to_integer(unsigned(idigit));
+                            k <= k + '1';
+                            state <= s2;
+                        end if;
+                   
+                   when s2 =>
+                        if idone = '1' then
+                            oready <= '0';
+                            state <= s3;
+                        end if;
+                        
+                        if idigit_valid = '1' then
+                            p_value := p(to_integer(unsigned(k)))
+                                        (to_integer(unsigned(idigit)));
+                            
+                            c <= d(c)(p_value);
+                            k <= k + '1';
+                        end if;
+                        
+                    when s3 => 
+                        if inv(c) = 0 then
+                            ocorrect <= '1';
+                        else
+                            oerror <= '1';
+                        end if;
+                        
+                        odone <= '1';
+                        state <= s0;
+                        
+                    when others =>  state <= s0;
+                end case;
+                
+            end if;
+        end if;
+    end process;
+
+end rtl;
